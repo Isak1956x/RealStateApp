a7819487be4221b490f4c7e540940c547cb293d3
Isak1956x <isaaccabrerasilverio12@gmail.com>
2025-08-04 11:43:56 -0400

    Property Entitties Added

---
+++ RealStateApp.Core.Domain/Entities/Offer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealStateApp.Core.Domain.Enums;
+
+namespace RealStateApp.Core.Domain.Entities
+{
+    public class Offer
+    {
+      
+            public int Id { get; set; }
+
+            public int ClientId { get; set; }
+
+            public int PropertyId { get; set; }
+            public Property? Property { get; set; }
+
+            public decimal Amount { get; set; }
+            public DateTime Date { get; set; }
+            public Status Status { get; set; } // "Pending", "Accepted", "Rejected"
+
+    }
+}
---
+++ RealStateApp.Core.Domain/Entities/PropertyImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealStateApp.Core.Domain.Entities
+{
+    public class PropertyImage
+    {
+        public int Id { get; set; }
+        public required string Url { get; set; }
+
+        public int PropertyId { get; set; }
+        public Property? Property { get; set; }
+    }
+}
