c881d93a5adeb5707869849791ba7f69e573de4d
CristXanderAza <cristopherxanderazadiaz19111@gmail.com>
2025-08-17 21:59:42 -0400

    Generic CQRS added

---
+++ RealStateApp.Core/Features/Common/GennericQueries/GetAllListQuery.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using RealStateApp.Core.Domain.Interfaces;
+
+namespace RealStateApp.Core.Application.Features.Common.GennericQueries
+{
+    public class GetAllListQuery<Tid, Tdto> : IRequest<IEnumerable<Tdto>>
+    {
+    }
+
+    public class GetAllListQueryHandler<Tid, TEntity, Tdto> : IRequestHandler<GetAllListQuery<Tid, Tdto>, IEnumerable<Tdto>>
+        where TEntity : class
+        where Tdto : class
+    {
+        private readonly IRepositoryBase<TEntity, Tid> _repository;
+        private readonly IMapper _mapper;
+        public GetAllListQueryHandler(IRepositoryBase<TEntity, Tid> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<Tdto>> Handle(GetAllListQuery<Tid, Tdto> request, CancellationToken cancellationToken)
+        {
+            var entities = await _repository.GetAllAsync();
+            return _mapper.Map<IEnumerable<Tdto>>(entities);
+        }
+    }
+}
---
+++ RealStateApp.Core/Features/Common/GennericQueries/GetByIdQuery.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using RealStateApp.Core.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealStateApp.Core.Application.Features.Common.GennericQueries
+{
+    public class GetByIdQuery<Tid, Tdto> : IRequest<Tdto> 
+    {
+        public Tid Id { get; set; } 
+    }
+
+    public class GetByIdQueryHandler<Tid,TEntity,TDto> : IRequestHandler<GetByIdQuery<Tid, TDto>, TDto>
+        where TEntity : class
+        where TDto : class
+    {
+        private readonly IRepositoryBase<TEntity, Tid> _repository;
+        private readonly IMapper _mapper;
+        public GetByIdQueryHandler(IRepositoryBase<TEntity, Tid> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<TDto> Handle(GetByIdQuery<Tid, TDto> request, CancellationToken cancellationToken)
+        {
+            var entity = await _repository.GetById(request.Id);
+            return _mapper.Map<TDto>(entity);
+        }
+    }
+}
